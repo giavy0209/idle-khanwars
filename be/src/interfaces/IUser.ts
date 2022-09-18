1aa4015a277557e5431c964ca5940aee36f9d0e4
giavy0209 <49144082+giavy0209@users.noreply.github.com>
2022-09-18 22:25:25 +0700

    a

--- be/src/interfaces/IUser.ts
+++
@@ -1,9 +0,0 @@
-import { Document, Types } from "mongoose"
-
-export default interface IUser extends Document{
-    username : string
-    password : string,
-    tenant : string
-    lastLogin : Date,
-    world : Types.ObjectId
-}
--- be/src/interfaces/IWorld.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose"
-
-export default interface IWorld extends Document{
-    name : string,
-    tenant : string,
-    speed : number
-}
